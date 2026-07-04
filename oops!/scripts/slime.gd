73166e05ec70e6a69dbe90b88b4def3f059df6e9
tanzz15 <162237511+tanzz15@users.noreply.github.com>
2026-07-04 16:52:15 +0700

    Add files via upload

--- oops!/scripts/slime.gd
+++ oops!/scripts/slime.gd
@@ -1,19 +1,18 @@
 extends Node2D
 
-@onready var area: Area2D = $Area2D
+const SPEED = 60
+var direction = 1
+
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-var muncul = false
-var posisi_awal
-
-func _ready():
-	posisi_awal = position
-	position.y += 32 # awalnya sembunyi di bawah tanah
-	area.body_entered.connect(_on_body_entered)
-
-func _on_body_entered(body):
-	if body.name == "Player" and !muncul:
-		muncul = true
-		var tween = create_tween()
-		tween.tween_property(self, "position", posisi_awal, 0.3)
-		animated_sprite.play("idle")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	position.x += direction * SPEED * delta
