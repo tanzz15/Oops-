38c8307539e6a5cde9346c03e46dcc6fae419aa7
tanzz15 <162237511+tanzz15@users.noreply.github.com>
2026-07-12 19:39:13 +0700

    Add files via upload

--- FP/Oops!/scripts/fruit.gd
+++ FP/Oops!/scripts/fruit.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+@onready var game_manager = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
 	print ("+1 fruit")
-	queue_free()
+	animation_player.play("pickup")
