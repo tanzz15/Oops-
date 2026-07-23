54091392686ce408e53c8ee320780597e39d26b4
tanzz15 <162237511+tanzz15@users.noreply.github.com>
2026-07-23 20:03:30 +0700

    Add files via upload

--- Oops!/scripts/game_manager.gd
+++ Oops!/scripts/game_manager.gd
@@ -6,13 +6,9 @@ var player : Player
 var score = 0
 var fruit = 0
 
-@onready var score_label = $ScoreLabel
-@onready var score_label_2: Label = $ScoreLabel2
-@onready var score_label_3: Label = $ScoreLabel3
-
+@onready var score_label: Label = $ScoreLabel
 @onready var fruit_label: Label = $FruitLabel
-@onready var fruit_label_2: Label = $FruitLabel2
-@onready var fruit_label_3: Label = $FruitLabel3
+
 
 func respawn_player():
 	if current_checkpoint != null and player != null:
@@ -22,11 +18,7 @@ func respawn_player():
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins."
-	score_label_2.text = "You collected " + str(score) + " coins."
-	score_label_3.text = "You collected " + str(score) + " coins."
 
 func add_fruit():
 	fruit += 1
 	fruit_label.text = "You collected " + str(fruit) + " fruits."
-	fruit_label_2.text = "You collected " + str(fruit) + " fruits."
-	fruit_label_3.text = "You collected " + str(fruit) + " fruits."
